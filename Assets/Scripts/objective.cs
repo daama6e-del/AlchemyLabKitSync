666f2d842ca48d38dc6c962f37cfc92ffc5015eb
David-Ammon-Student <daama6e@student.oaisd.org>
2026-03-31 21:33:52 -0400

    Start at minute 9.

--- Assets/Scripts/objective.cs
+++ Assets/Scripts/objective.cs
@@ -1,12 +1,24 @@
-
+using UnityEngine.Events;
 using UnityEngine;
 
 public class objective : MonoBehaviour
 {
     [SerializeField] private string objectiveText = "Thou shall have Glorious quest!";
+    [SerializeField] private string completedText = "Glory has been awarded to thee Champion!!!";
+
+    public UnityEvent onObjectiveComplete = new UnityEvent();
 
     private void OnEnable()
     {
         Debug.Log(objectiveText);
-    } 
+    }
+
+    public void CompleteObjective()
+    {
+        Debug.Log(completedText);
+
+        onObjectiveComplete.Invoke(); // Start of the new Quest, hands tied.
+
+        gameObject.SetActive(false); // Deactivates the quest after achivement.
+    }
 }
